e6f1368f2c25f30a2b96cdda45ae47470ff60d01
John Shaffer <john@insilica.co>
2023-12-06 15:38:26 -0600

    Add dev-shell

---
+++ flake.nix
@@ -0,0 +1,7 @@
+{
+  description = "stringDB BioBrick";
+
+  inputs = { dev-shell.url = "github:biobricks-ai/dev-shell"; };
+
+  outputs = { self, dev-shell }: { devShells = dev-shell.devShells; };
+}
